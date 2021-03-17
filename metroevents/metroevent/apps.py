5a79368f7eadff09f6cdd41989a0e59ed9808a7f
Diane Isabelle Lambojo <72972840+dianelambojo@users.noreply.github.com>
2021-03-17 23:45:16 +0800

    djangoproject

---
+++ metroevents/metroevent/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MetroeventConfig(AppConfig):
+    name = 'metroevent'
---
+++ metroevents/metroevent/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+from django.conf.urls.static import static
+from django.conf import settings
+
+#paths arranged alphabetically by name
+app_name = 'metroevent'
+urlpatterns = [ 
+    # path('api/data', views.get_data, name='api-data'),
+
+    #TEST URL
+    path('login/', views.LoginView.as_view(), name="LoginView")
+
+]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
